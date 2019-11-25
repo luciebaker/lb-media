8eb167f02dd37835fb8cffd43e2ca3fcf01f5a35
luciebaker <luciebdev@gmail.com>
2019-11-25 02:20:59 -0500

    added header and box sections

---
+++ src/components/Homepage/Header.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+export default class Header extends Component {
+    render() {
+        return (
+        <div className="container-fluid">
+        <h1 className='header-title'>Creating</h1> 
+        <h1 className='header-title-highlight'>State of the art</h1>
+        <h1 className='header-subtitle'>websites</h1>
+        </div>
+        )
+    }
+}
+
+
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,16 @@
 import React from "react"
 import { FaGithub } from 'react-icons/fa'
 import Layout from "../components/layout"
+import Header from "../components/Homepage/Header"
+import Box from "../components/Homepage/Box"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1 className="text-center">Hello there!</h1>
-    <h1 className="text-center">Hello there!</h1>
+    <Header />
+    <Box />
+    <h1 className="text-center mt-5">Hello there!</h1>
     <h3 className="text-center">You've found our website but there's nothing here yet... the sadness</h3>
     <h1 className="text-center">It's ok.... We're building an awesome website</h1>
     <h3 className="text-center md-10">Visit again soon  <FaGithub />.</h3>
